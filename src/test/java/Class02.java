cf087f6ff0877df1f339d808499ef8de35da06b0
Anand <anand@gmail.com>
2026-01-30 09:19:06 +0530

    "Class02 Automation Branch"

---
+++ src/test/java/Class02.java
@@ -0,0 +1,10 @@
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cllass02 is created in Automation branch");
+
+	}
+
+}
